dd16ba70c794d1a043d3dc6a02d9eb039421774e
Tomas Samak <samaktomas@gmail.com>
2023-07-20 16:48:09 +0200

    initial commit

---
+++ actions/get-total-revenue.ts
@@ -0,0 +1,27 @@
+import prismadb from "@/lib/prismadb";
+
+export const getTotalRevenue = async (storeId: string) => {
+  const paidOrders = await prismadb.order.findMany({
+    where: {
+      storeId,
+      isPaid: true,
+    },
+    include: {
+      orderItems: {
+        include: {
+          product: true,
+        },
+      },
+    },
+  });
+
+  const totalRevenue = paidOrders.reduce((acc, order) => {
+    const orderTotal = order.orderItems.reduce((orderAcc, item) => {
+      return orderAcc + item.product.price.toNumber();
+    }, 0);
+
+    return acc + orderTotal;
+  }, 0);
+
+  return totalRevenue;
+};
